b604a30cb765a6109cfc0f65699c05c07e84019f
Chibuzo <chibuzo.henry@gmail.com>
2018-09-04 01:02:29 +0100

    Add project files.

---
+++ AngryUsers.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AngryUsers;
+using AngryUsers.Controllers;
+
+namespace AngryUsers.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Home Page", result.ViewBag.Title);
+        }
+    }
+}
---
+++ AngryUsers/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AngryUsers.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
