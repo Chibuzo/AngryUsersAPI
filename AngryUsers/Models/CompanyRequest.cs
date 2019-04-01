8ff32523b63acff6d3bce83674a1db51664126b9
Chibuzo <chibuzo.henry@gmail.com>
2019-04-01 01:58:04 +0100

    Claim account request

---
+++ AngryUsers/Models/CompanyRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AngryUsers.Models
+{
+    public class CompanyRequest
+    {
+        public int Id { get; set; }
+        [Required]
+        public string ContactPerson { get; set; }
+        [Required]
+        public string ContactPhone { get; set; }
+        [Required]
+        public string ContactEmail { get; set; }
+        public string CompanyName { get; set; }
+        [Required]
+        public int CompanyId { get; set; }
+        public virtual Company Company { get; set; }
+        public string Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
